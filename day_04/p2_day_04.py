f70c1d2195783ede3a776d10275ddca9073056c7
William Y. Feng <womogenes2@gmail.com>
2024-12-04 00:55:34 -0500

    Refactor day 4 part 2

--- day_04/p2_day_04.py
+++ day_04/p2_day_04.py
@@ -11,7 +11,7 @@ for dx in range(-1, 2):
             dd.append((dx, dy))
 
 def has_xmas(i, j):
-    if not (1 <= i < n - 1 and 1 <= j < n - 1):
+    if not (1 <= i < n - 1 and 1 <= j < m - 1):
         return False
     if lines[i][j] != "A":
         return False
@@ -20,9 +20,7 @@ def has_xmas(i, j):
     diag_1 = f"{lines[i-1][j-1]}{lines[i+1][j+1]}"
     diag_2 = f"{lines[i-1][j+1]}{lines[i+1][j-1]}"
 
-    if diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]:
-        return True
-    return False
+    return diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]
 
 ans = 0
 for i in range(n):
